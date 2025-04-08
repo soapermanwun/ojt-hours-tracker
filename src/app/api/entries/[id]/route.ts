a39f5a17edfbd1ae43fd39eb65ab72ed32fdcf42
soapermanwun <203548283+soapermanwun@users.noreply.github.com>
2025-04-08 23:48:45 +0800

    feat: add entries api and modules

---
+++ src/app/api/entries/[id]/route.ts
@@ -0,0 +1,56 @@
+import { deleteEntry, getEntriesByID } from "@/app/modules/entries";
+import { NextRequest } from "next/server";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: number }> }
+) {
+  const { id } = await params;
+  const searchParams = request.nextUrl.searchParams;
+  const created_by = searchParams.get("created_by");
+
+  if (!created_by) {
+    return new Response(
+      JSON.stringify({ error: "The requested resource could not be found" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  if (!id) {
+    return new Response(
+      JSON.stringify({ error: "The requested resource could not be found" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  try {
+    const existingEntry = await getEntriesByID(Number(id), created_by);
+
+    if (!existingEntry) {
+      return new Response(
+        JSON.stringify({ error: "The requested resource could not be found" }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    await deleteEntry(Number(id), created_by);
+
+    return new Response(null, {
+      status: 204,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Unknown error occurred" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
---
+++ src/app/modules/entries/repository.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/utils/prisma";
+import { Entries } from "../../../../generated/prisma";
+
+export async function getEntriesByUser(uuid: string): Promise<Entries[]> {
+  return await prisma.entries.findMany({
+    where: {
+      created_by: uuid,
+    },
+  });
+}
+
+export async function getEntriesByID(
+  id: number,
+  uuid: string
+): Promise<Entries | null> {
+  return await prisma.entries.findUnique({
+    where: {
+      id,
+      created_by: uuid,
+    },
+  });
+}
+
+export async function createEntries(
+  data: Omit<Entries, "id" | "created_at">
+): Promise<Entries> {
+  return await prisma.entries.create({ data });
+}
+
+export async function deleteEntry(id: number, uuid: string): Promise<Entries> {
+  return await prisma.entries.delete({
+    where: {
+      id,
+      created_by: uuid,
+    },
+  });
+}
